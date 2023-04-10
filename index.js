d52e43dff3986585132d020d2322cb503ac9cfca
coboooo <50066465+coboooo@users.noreply.github.com>
2023-04-10 16:42:53 +0900

    1

--- index.js
+++ index.js
@@ -1,60 +1,78 @@
-var express = require('express');
+const nodemailer = require("nodemailer");
+var express = require("express");
 var app = express();
 
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
+  dialect: "sqlite",
+  storage: "database.sqlite",
 });
 
-const Comments = sequelize.define('Comments', {
-  content: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}, {
-});
+const Comments = sequelize.define(
+  "Comments",
+  {
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {}
+);
 
 (async () => {
-await Comments.sync();
+  await Comments.sync();
 })();
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', async function(req, res) {
+app.get("/", async function (req, res) {
   const comments = await Comments.findAll();
-  res.render('index',{ comments: comments});
+  res.render("index", { comments: comments });
 });
 
-app.post('/create', async function(req, res) {
-  const { content } = req.body
+app.post("/create", async function (req, res) {
+  const { content } = req.body;
   await Comments.create({ content: content });
-  res.redirect('/')
+  res.redirect("/");
 });
 
-app.post('/update/:id', async function(req, res) {
-  const { content } = req.body
-  const { id } = req.params
-  await Comments.update({ content: content }, {
-    where: {
-      id: id
+app.post("/update/:id", async function (req, res) {
+  const { content } = req.body;
+  const { id } = req.params;
+  await Comments.update(
+    { content: content },
+    {
+      where: {
+        id: id,
+      },
     }
-  });
-  res.redirect('/')
+  );
+  res.redirect("/");
 });
 
-app.post('/delete/:id', async function(req, res) {
-  const { id } = req.params
+app.post("/delete/:id", async function (req, res) {
+  const { id } = req.params;
   await Comments.destroy({
     where: {
-      id: id
-    }
+      id: id,
+    },
   });
-  res.redirect('/')
+  res.redirect("/");
 });
 
 app.listen(3000);
-console.log('Server is listening on port 3000');
+console.log("Server is listening on port 3000");
+
+///////////////////////////////////////////
+
+const now = new Date();
+const timeTo3 =
+  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 35, 0) - now;
+if (timeTo3 > 0) {
+  setTimeout(function () {
+    console.log("되었습니다!");
+  }, timeTo3);
+}
